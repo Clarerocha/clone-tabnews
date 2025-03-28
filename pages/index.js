f93fa57ab053ac1716dc1d91261341d35cdf13c1
Claudia Rocha <123769581+Clarerocha@users.noreply.github.com>
2025-03-28 17:33:49 +0000

    modifiquei a frase inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Piu,Nós vamos Vencer! Tenha Fé! Se você concorda,diga Piu!</h1>
+    return <h1>Piu,Nós vamos Vencer! Tenha Fé! Se você concorda,diga Piu,🐥 Piu🐥!</h1>
 }
 
 export default Home;
