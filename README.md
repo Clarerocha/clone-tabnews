17b547d21fc49176ce87a2e4cdeb2b0043dabae2
Claudia Rocha <123769581+Clarerocha@users.noreply.github.com>
2025-04-10 17:50:42 +0000

    adiciona scripts `lint:check`e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Criando um projeto do zero com o curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 function Home() {
-    return <h1>Piu,Nós vamos Vencer! Tenha Fé! Se você concorda,diga Piu,🐥 Piu🐥!</h1>
+  return (
+    <h1>Piu,Nós vamos Vencer! Tenha Fé! Se você concorda,diga Piu,🐥 Piu🐥!</h1>
+  );
 }
 
+function teste() {}
+
+function teste2() {
+  console.log("indentação errada");
+}
 export default Home;
